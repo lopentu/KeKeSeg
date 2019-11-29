2f892ca163801ae805ad9c850a4960bf3f524b44
seantyh <seantyh@gmail.com>
2019-11-29 13:55:05 +0800

    update dependencies

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
-    install_requires=["zhon"],
+    install_requires=["zhon", "nltk"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
